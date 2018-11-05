64c2bd38e59fa52ee21da4bb16c0ddd661b354bd
Karol Pawelski <karol.pawelski@cognifide.com>
2018-11-05 22:38:14 +0100

    create and just factories

---
+++ src/main/java/Example_1_CreateFactory.java
@@ -0,0 +1,43 @@
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class Example_1_CreateFactory {
+
+    public static void main(String[] args) {
+        Observable<String> myObservable = Observable.create(emitter -> {
+            try {
+                emitter.onNext("first value");
+                emitter.onNext("second value");
+                emitter.onNext("third value");
+                emitter.onComplete();
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        });
+
+        Observer<String> myObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("completed!");
+            }
+        };
+
+        myObservable.subscribe(myObserver);
+    }
+
+}
